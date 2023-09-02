1099312f7a760a64de5dbd08d832bfe206fa2efd
rabbi-hosen <rabbihosen01581@gmail.com>
2023-09-02 21:47:17 +0600

    conditional readering

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//import STATE from './STATE'
+import CONDITIONAL_RENDERING from './CONDITIONAL_RENDERING/index'
+
+
+export default function App() {
+  return (
+    <div>
+       < CONDITIONAL_RENDERING />
+       
+    </div>
+  )
+}
+
+
